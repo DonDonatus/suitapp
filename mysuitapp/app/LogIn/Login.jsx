91a90a28a4380536ae9d5df178f563989cf2aec1
pius-code <obliepius13@gmail.com>
2025-01-22 13:43:02 +0000

    Update global styles, modify metadata, and implement Google authentication in login flow

--- mysuitapp/app/LogIn/Login.jsx
+++ mysuitapp/app/LogIn/Login.jsx
@@ -2,11 +2,13 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGoogle } from "@fortawesome/free-brands-svg-icons";
+import { signIn } from "next-auth/react";
 
 const Login = () => {
   return (
-    <div className="h-[100vh] w-full text-black bg-[#fafafa] flex">
+    <div className="h-[65vh] lg:h-[70vh] w-full text-black bg-[#fafafa] flex   items-end lg:items-start  ">
       {/* Image container */}
       <div className="w-[40%] hidden lg:block">
         <Image
@@ -22,7 +24,7 @@ const Login = () => {
         <h1 className="text-3xl text-center font-bold font-sans text-gray-800">
           Sign in
         </h1>
-        <p className="text-center text-sm mt-2 font-light text-gray-600">
+        <p className="text-center text-sm mt-2 font-light text-gray-600 ">
           Enter email and password to sign in.
         </p>
         <form className="w-[80%] mt-4 flex flex-col items-center">
@@ -40,22 +42,20 @@ const Login = () => {
             aria-label="Password"
             className="w-[70%] p-2 mb-4 border border-black rounded-[5px] focus:outline-none focus:border-black"
           />
-          <div className="remember flex w-[70%]  h-10 justify-center items-center">
-            <input
-              type="checkbox"
-              name="Remember me"
-              id="Remember"
-              className="h-7 w-7 border border-black rounded-[5px] focus:outline-none focus:border-black mr-3 cursor-pointer"
-            />
-            <p className="w-full text-gray-600 font-thin text-sm my-2">
-              Remember me?
-            </p>
-          </div>
 
           <button className="w-[70%] p-2 bg-[#4c6474] text-white rounded-lg hover:bg-gray-800 active:scale-95 transition-all my-2">
             Sign in
           </button>
         </form>
+        <p>Or</p>
+        <button
+          className="w-[56%] p-2 bg-[#afbdc600] text-black rounded-lg  active:scale-95 transition-all my-2 border border-black hover:bg-[#ad723fbc] hover:text-white"
+          onClick={() => signIn("google")}
+        >
+          <FontAwesomeIcon icon={faGoogle} className="mr-2 text-[#ff4d00]" />
+          Sign in with Google
+        </button>
+
         <p className="text-gray-600 text-sm mt-2">
           Don't have an account?{" "}
           <Link href="../Register" className="text-blue-600">
--- mysuitapp/app/page.js
+++ mysuitapp/app/page.js
@@ -1,9 +1,13 @@
 import React from "react";
 import Login from "./LogIn/Login";
+// Import the Font Awesome core
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css"; // Import the CSS
+config.autoAddCss = false; // Prevents Font Awesome from adding its own CSS
 
 const page = () => {
   return (
-    <div className="h-[100vh] w-full bg-red-50 text-black">
+    <div className="h-[100vh] w-full bg-[#fafafa] text-black ">
       <Login />
     </div>
   );
